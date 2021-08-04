9d07828d56801837b40054a0a756f438d24d6c7a
Josh Braendel <Jbud@live.ca>
2021-08-03 22:46:17 -0400

    not quite

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,7 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import Bold from '@tiptap/extension-bold'
-import Italics from '@tiptap/extension-bold'
+import Italics from '@tiptap/extension-italic'
 import HardBreak from '@tiptap/extension-hard-break'
 
 window['Vue'] = Vue;
