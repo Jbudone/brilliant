f50fda816a9544ff167d3db50f5b348659c5d0c2
Josh Braendel <Jbud@live.ca>
2021-10-01 20:03:29 -0400

    new logo/name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Lxndria'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://brilliant.local'),
+    'url' => env('APP_URL', 'http://lxndria.local'),
 
     'asset_url' => env('ASSET_URL', null),
 
