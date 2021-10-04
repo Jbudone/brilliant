ab836b2188f57538badeb578f000320d012f21dd
Josh Braendel <Jbud@live.ca>
2021-10-03 22:53:37 -0400

    mention vue

---
+++ resources/js/mention.vue
@@ -0,0 +1,42 @@
+<template>
+    Name
+</template>
+
+<script>
+export default {
+    name: 'Mention',
+    template: 'Mention',
+    components: { },
+    emits: [],
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        link: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+
+    beforeUnmount() {
+
+    },
+};
+</script>
+
+<style>
+</style>
